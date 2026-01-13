b90e0f69f9ae635e9ceb552e4ac76fa05b3ad034
Aayush0007 <sharmaaayush2310@gmail.com>
2026-01-14 01:12:40 +0530

    asd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -190,7 +190,7 @@ export default function Footer() {
                 className="text-[10px] leading-relaxed italic uppercase tracking-wider font-medium"
                 style={{ color: COLORS.tan }}
               >
-                RC/HARERA/GGM/3846/3441/2025/723
+                RC/HARERA/GGM/3846/3441/2025/723 |
                 RERA IDs: RC/REP/HARERA/GGM/976/708/2025/79 |
                 RC/REP/HARERA/GGM/993/725/2025/96 |
                 RC/REP/HARERA/GGM/917/649/2025/20.
